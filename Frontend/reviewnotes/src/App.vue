fed45efc647078a899684e57ccf7da23dae321d4
MrMelk <aretov@live.com>
2022-10-04 18:22:56 +0200

    add breadcrumbs as a component instead of in app.vue

--- Frontend/reviewnotes/src/App.vue
+++ Frontend/reviewnotes/src/App.vue
@@ -1,19 +1,41 @@
 <template>
-  <div class="app">
-    <p>
-      
-    </p>
-  </div>
+  <v-app>
+    
+    <v-main>
+      <BreadCrumbs/>
+     <div>
+      <v-row no gutters>
+        <v-col>
+        <h1 class="header">Review notes</h1>
+        </v-col>
+      <v-col>
+        <button class="btn">New note</button>
+      </v-col>
+      </v-row>
+      <div class="toolbar">
+        sdsd
+      </div>
+     </div>
+    </v-main>
+  </v-app>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import BreadCrumbs from "./components/BreadCrumbs.vue"
 
 export default defineComponent({
   name: 'App',
+  
   components: {
-  }
-});
+    BreadCrumbs
+  },
+  data () {
+    return {
+      
+    }
+  },
+})
 </script>
 
 <style>
---
+++ Frontend/reviewnotes/src/components/BreadCrumbs.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+    <v-breadcrumbs 
+    :items="items"
+    divider="/">
+    </v-breadcrumbs>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+    
+export default defineComponent({
+  name: 'BreadCrumbs',
+
+    data () {
+      return {
+        items: [{
+          text: 'Engagements',
+          disabled: false,
+          href: '/engagements/2021_001/reviewnotes',
+        },
+        {
+          text: 'Microsoft 2021',
+          disabled: false,
+          href: '/engagements/2021_001/reviewnotes',
+        },
+        {
+          text: 'reviewnotes',
+          disabled: true,
+          href: '/engagements/2021_001/reviewnotes',
+        },
+      ],
+      }
+    },
+  })
+</script>
